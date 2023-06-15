b23d91c843ab19091b7edf105f01703e0b7f155a
markmcsherry <mark.mcsherry@sap.com>
2023-06-15 13:55:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Page() {
-  return <h1>Hello, World...</h1>;
+  return (
+    <div id="page">
+      <div id="header">
+        <h1>EMT PROMISES</h1>
+      </div>
+      <div id="body">
+        <h2>Aide Memoire</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, tellus in pellentesque varius, nibh enim convallis lorem, a sollicitudin dolor turpis non leo. Praesent dignissim libero vitae accumsan interdum. Nulla rutrum eget nibh ac gravida. Morbi ornare purus a felis lacinia scelerisque. Nullam at nibh in risus cursus condimentum id ac nisl. Donec lacinia mi ante, sollicitudin rhoncus orci ullamcorper nec. Vestibulum pulvinar sem lacus, vitae varius ipsum sodales eu.</p>
+        <h2>Quizes</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, tellus in pellentesque varius, nibh enim convallis lorem, a sollicitudin dolor turpis non leo. Praesent dignissim libero vitae accumsan interdum. Nulla rutrum eget nibh ac gravida. Morbi ornare purus a felis lacinia scelerisque. Nullam at nibh in risus cursus condimentum id ac nisl. Donec lacinia mi ante, sollicitudin rhoncus orci ullamcorper nec. Vestibulum pulvinar sem lacus, vitae varius ipsum sodales eu.</p>
+        <h2>Links</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, tellus in pellentesque varius, nibh enim convallis lorem, a sollicitudin dolor turpis non leo. Praesent dignissim libero vitae accumsan interdum. Nulla rutrum eget nibh ac gravida. Morbi ornare purus a felis lacinia scelerisque. Nullam at nibh in risus cursus condimentum id ac nisl. Donec lacinia mi ante, sollicitudin rhoncus orci ullamcorper nec. Vestibulum pulvinar sem lacus, vitae varius ipsum sodales eu.</p>
+      </div>
+    </div>
+    );
 }
